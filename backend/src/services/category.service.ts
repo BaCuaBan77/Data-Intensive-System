5b605c9691b9bb1ff85342eb590645c2f02148a1
elidare <elidare.jjj@gmail.com>
2025-12-11 22:53:59 +0200

    Refactoring

--- backend/src/services/category.service.ts
+++ backend/src/services/category.service.ts
@@ -1,17 +1,12 @@
-import { QueryConfig } from "pg";
 import { queryFromPrimary } from "./database"
 import { CategorySchema, Category } from "../schemas/category.schema";
 
-export const getCategoriesFromPrimaryDb = async (
-  sqlQuery: QueryConfig
-): Promise<Category[]> => {
-  return queryFromPrimary(sqlQuery, CategorySchema);
-};
-
 export const getAllCategories = async (): Promise<Category[]> => {
   // Get only from Primary DB as data is identical in both
-  return await getCategoriesFromPrimaryDb({
+  const sqlQuery = {
     text: `SELECT * FROM categories;`,
     values: [],
-  });
+  };
+
+  return await queryFromPrimary(sqlQuery, CategorySchema);
 };
